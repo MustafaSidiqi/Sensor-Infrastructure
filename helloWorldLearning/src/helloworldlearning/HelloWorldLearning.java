dd2cc469f15413133790406ef895678d1c6e4e63
Niklas Buhl <nibuh@dtu.dk>
2017-03-10 16:33:56 +0100

    tests

--- helloWorldLearning/src/helloworldlearning/HelloWorldLearning.java
+++ helloWorldLearning/src/helloworldlearning/HelloWorldLearning.java
@@ -5,6 +5,9 @@
  */
 package helloworldlearning;
 
+import java.io.IOException;
+
+
 /**
  *
  * @author nb
@@ -13,11 +16,19 @@ public class HelloWorldLearning {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         
         System.out.println("Hello World!");
+        
+        try { 
+            Process process = Runtime.getRuntime().exec("wall Hej Jesper, hilsen Java Programmet.");
+        } catch (IOException ex) {
+        }
+        
+        System.out.println("End.");
     }
     
 }
