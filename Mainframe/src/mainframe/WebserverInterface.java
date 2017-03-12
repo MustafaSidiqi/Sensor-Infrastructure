0a7c3883be370d54eb9a48de61641ec8008d2603
LenovoYogaPro2\Mustafa <mustafasidiqi95@gmail.com>
2017-03-12 16:51:51 +0100

    Fixed errors

--- Mainframe/src/mainframe/WebserverInterface.java
+++ Mainframe/src/mainframe/WebserverInterface.java
@@ -12,12 +12,10 @@ import java.rmi.RemoteException;
  * @author nb
  */
 public interface WebserverInterface extends java.rmi.Remote {
+
     
-<<<<<<< HEAD
-    public boolean cyberLogin(String username, String password) throws RemoteException;
-=======
     public boolean cyberLogin(String username, String password) throws java.rmi.RemoteException;
->>>>>>> f5bfdc86cd154d16ca2978bee36e17f88faac632
+
     
     //public void setPassword(String password) throws RemoteException;    
 
