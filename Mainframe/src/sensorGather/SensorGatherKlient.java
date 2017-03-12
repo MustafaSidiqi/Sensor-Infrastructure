0e18e2e13559913fe83a946f04f559e7d30cfcf4
triblex <msc95x@gmail.com>
2017-03-12 13:56:08 +0100

    Working Remote sensor
    server and client working

--- Mainframe/src/sensorGather/SensorGatherKlient.java
+++ Mainframe/src/sensorGather/SensorGatherKlient.java
@@ -1,16 +1,20 @@
 package sensorGather;
 
 import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
 import java.util.ArrayList;
 //import java.util.Scanner;
 
 public class SensorGatherKlient {
     static int indexMax = 0;
     public static void main(String[] args) throws Exception {
+        //System.setSecurityManager(new RMISecurityManager());
+
+
         ArrayList<Integer> data = new ArrayList<>();
         
         //boolean aktiv = true; // Boolean til at se om spillet skal spilles
-        SensorGatherI g = (SensorGatherI) Naming.lookup("rmi://localhost:1099/SensorGatherService");
+        SensorGatherI g = (SensorGatherI) Naming.lookup("rmi://ubuntu4.javabog.dk:53712/s153712");
 
         g.getSensorData();
         
@@ -29,10 +33,10 @@ public class SensorGatherKlient {
         System.out.println("Username = " + g.getUsername());
         System.out.println("Password = " + g.getPassword());
         System.out.println("Data amount = " + g.getDataAmount());
-        System.out.println("Data amount = " + g.getID());
-        System.out.println("Data amount = " + g.getType());
-        System.out.println("Data amount = " + g.getName());
-        System.out.println("Data amount = " + g.getUnit());
+        System.out.println("ID = " + g.getID());
+        System.out.println("Type = " + g.getType());
+        System.out.println("Name = " + g.getName());
+        System.out.println("Unit = " + g.getUnit());
         
         
     }
