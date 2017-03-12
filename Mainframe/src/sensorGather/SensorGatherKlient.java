acf366a59a15f460bf471a661c6572490f0e0177
triblex <msc95x@gmail.com>
2017-03-12 16:39:12 +0100

    SensorGather klient updated

--- Mainframe/src/sensorGather/SensorGatherKlient.java
+++ Mainframe/src/sensorGather/SensorGatherKlient.java
@@ -2,6 +2,7 @@ package sensorGather;
 
 import java.rmi.Naming;
 import java.rmi.RMISecurityManager;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Random;
 import mainframe.SensorInterface;
@@ -9,74 +10,34 @@ import static sensorGather.SensorGatherServer.timeStamp;
 //import java.util.Scanner;
 
 public class SensorGatherKlient {
+
     static int indexMax = 0;
+    static String username = "";
+    static String password = "";
+    private static final String sensorID = "0001";
+    private static final String sensorType = "Temperature";
+    private static final String sensorDataType = "Integer";
+    private static final String sensorUnit = "Celcius";
+    private static final String sensorName = "Temperature Data";
+
     public static void main(String[] args) throws Exception {
         //System.setSecurityManager(new RMISecurityManager());
 
-
-        //ArrayList<String> data = new ArrayList<>();
-        
-        //boolean aktiv = true; // Boolean til at se om spillet skal spilles
-        //SensorGatherI g = (SensorGatherI) Naming.lookup("rmi://ubuntu4.javabog.dk:53720/s153712");
+        //System.setSecurityManager(new RMISecurityManager());
         //SensorGatherI g = (SensorGatherI) Naming.lookup("rmi://localhost/kontotjeneste");
-        
-        SensorInterface g = (SensorInterface) Naming.lookup("rmi://52.56.199.233/sensorcommandcenter");
-
-
-        //
-        
-        /*
-        g.getSensorData();
-        
-        for (String temp : g.getSensorData()) {
-            data.add(temp);
-            indexMax++;
-        }
-        
-        for(int i = 0; i < data.size();i++){
-        System.out.println(data.get(i));
-        }
-
-        */
-        
-        String bruger = "s153712";
-        String pass = "yas12!";
+        SensorInterface g = (SensorInterface) Naming.lookup("rmi://52.56.199.233:1337/sensorcommandcenter");
         String stringData = "";
-        //g.setLogin(bruger, pass);
 
-        
-        
         while (true) {
- 
+            timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
             Random rand = new Random();
             int value = rand.nextInt(20);
             Thread.sleep(3000);
 
-            stringData = g.addTogether().concat(Integer.toString(value)).concat(" ").concat(timeStamp);
-            //g.storeData(stringData);
-            //g.storeData(value);
-            System.out.println("String :" + stringData);
-            //g.clearDataArray();
+            stringData = "1 LYNGBY ".concat(sensorType).concat(" ").concat(sensorUnit).concat(" ").concat(Integer.toString(value)).concat(" ").concat(timeStamp);
+            g.transferData(username, password, stringData);
             
-            g.transferData(bruger,pass,stringData);
-
-            //Integer.toString(i)
-            //"to".concat("get").concat("her")
-            //
+            //System.out.println("String :" + stringData);
         }
-        
-        /*
-        
-        //System.out.println("Username = " + g.getUsername());
-        //System.out.println("Password = " + g.getPassword());
-        System.out.println("Data amount = " + g.getDataAmount());
-        System.out.println("ID = " + g.getID());
-        System.out.println("Type = " + g.getType());
-        System.out.println("Name = " + g.getName());
-        System.out.println("Unit = " + g.getUnit());
-
-        */
-        
-        
     }
 }
