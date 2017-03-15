b526a800276e417e129cf40fb0c2a166bb5b9022
triblex <msc95x@gmail.com>
2017-03-15 13:01:11 +0100

    Deleted some unused sensor shiat

--- Mainframe/src/sensorGather/SensorGatherKlient.java
+++ Mainframe/src/sensorGather/SensorGatherKlient.java
@@ -5,28 +5,27 @@ import java.rmi.RMISecurityManager;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Random;
-import mainframe.SensorInterface;
-import static sensorGather.SensorGatherServer.timeStamp;
+//import mainframe.SensorInterface;
 //import java.util.Scanner;
 
 public class SensorGatherKlient {
 
     static int indexMax = 0;
-    static String username = "";
-    static String password = "";
-    private static final String sensorID = "0001";
+    static String username = "s153712";
+    static String password = "yas12!";
+    private static final String sensorID = "1";
     private static final String sensorType = "Temperature";
-    private static final String sensorDataType = "Integer";
     private static final String sensorUnit = "Celcius";
-    private static final String sensorName = "Temperature Data";
-
+    private static final String sensorLocation = "Lyngby";
+    
+    static String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+    
     public static void main(String[] args) throws Exception {
         //System.setSecurityManager(new RMISecurityManager());
 
-        //System.setSecurityManager(new RMISecurityManager());
         //SensorGatherI g = (SensorGatherI) Naming.lookup("rmi://localhost/kontotjeneste");
         SensorInterface g = (SensorInterface) Naming.lookup("rmi://52.56.199.233:1337/sensorcommandcenter");
-        String stringData = "";
+        String stringData;
 
         while (true) {
             timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
@@ -34,7 +33,7 @@ public class SensorGatherKlient {
             int value = rand.nextInt(20);
             Thread.sleep(3000);
 
-            stringData = "1 LYNGBY ".concat(sensorType).concat(" ").concat(sensorUnit).concat(" ").concat(Integer.toString(value)).concat(" ").concat(timeStamp);
+            stringData = sensorID.concat(" ").concat(sensorLocation).concat(" ").concat(sensorType).concat(" ").concat(sensorUnit).concat(" ").concat(Integer.toString(value)).concat(" ").concat(timeStamp);
             g.transferData(username, password, stringData);
             
             //System.out.println("String :" + stringData);
---
+++ Mainframe/src/sensorGather/SensorInterface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sensorGather;
+
+import mainframe.*;
+
+/**
+ *
+ * @author nb
+ */
+public interface SensorInterface extends java.rmi.Remote {
+    
+    boolean transferData(String username, String password, String data) throws java.rmi.RemoteException;
+    boolean isThereNewData() throws java.rmi.RemoteException;
+    public String getData() throws java.rmi.RemoteException;
+
+    //public Object addTogether();
+    
+}
