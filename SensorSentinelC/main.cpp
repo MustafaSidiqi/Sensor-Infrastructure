0b93803b7d8a30476eb3fccaf4a9d6fedb992a85
Niklas Buhl <nibuh@dtu.dk>
2017-03-16 16:53:37 +0100

    dunno

--- SensorSentinelC/main.cpp
+++ SensorSentinelC/main.cpp
@@ -11,15 +11,34 @@
  * Created on February 28, 2017, 1:51 PM
  */
 
+#define AMAZON_SERVER "www.google.com"
+
 #include <cstdlib>
+#include <cpprest/json.h>
+//#include <cpprest/http_client.h>
+//#include <cpprest/filestream.h>
+//#include <iostream>
+//#include <streambuf>
+//#include <sstream>
+//#include <fstream>
+//#include <map>
+//#include <vector>
+//#include <string>
+//#include <exception>
+
 
 using namespace std;
+//using namespace utility;
+//using namespace web;
+//using namespace http;
+//using namespace http::client;
+//using namespace concurrency::streams;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
-
+    
     return 0;
+
 }
+
+
 
