0bd98162ba9da5b2928c78c790247d6d7a1b997f
triblex <msc95x@gmail.com>
2017-04-19 13:54:30 +0200

    Sensor og userauthentication
    Det hele er sat ind, men der skal tweekes lidt før det virker.

--- FinalForm/SensorServer/src/sensorserver/SensorInterface.java
+++ FinalForm/SensorServer/src/sensorserver/SensorInterface.java
@@ -3,20 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mainframe;
-
-//import mainframe.*;
+package sensorserver;
 
 /**
  *
- * @author nb
+ * @author MSC
  */
 public interface SensorInterface extends java.rmi.Remote {
     
     boolean transferData(String username, String password, String data) throws java.rmi.RemoteException;
-    //boolean isThereNewData() throws java.rmi.RemoteException;
-    //public String getData() throws java.rmi.RemoteException;
+    boolean isThereNewData() throws java.rmi.RemoteException;
+    public String getData() throws java.rmi.RemoteException;
 
-    //public Object addTogether();
-    
 }
