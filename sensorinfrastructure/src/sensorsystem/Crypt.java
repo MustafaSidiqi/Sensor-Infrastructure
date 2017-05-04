3f5ad26e0796370d46d5d495d103fea5e48199d1
triblex <msc95x@gmail.com>
2017-05-04 12:59:45 +0200

    Fixed crypt

--- sensorinfrastructure/src/sensorsystem/Crypt.java
+++ sensorinfrastructure/src/sensorsystem/Crypt.java
@@ -5,26 +5,56 @@
  */
 package sensorsystem;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Random;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 
 /**
  *
  * @author triblex
  */
 public class Crypt {
-    public byte[] encrypt(String plainText, String encryptionKey, String IV) throws Exception {
-    Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
-    SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
-    cipher.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
-    return cipher.doFinal(plainText.getBytes("UTF-8"));
-  }
+    private static final String ALGORITMO = "AES/CBC/PKCS5Padding";
+    private static final String CODIFICACION = "UTF-8";
+    
+    public static String encrypt(String plaintext, String key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
+        byte[] raw = DatatypeConverter.parseHexBinary(key);
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        Cipher cipher = Cipher.getInstance(ALGORITMO);
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+        byte[] cipherText = cipher.doFinal(plaintext.getBytes(CODIFICACION));
+        byte[] iv = cipher.getIV();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        outputStream.write(iv);
+        outputStream.write(cipherText);
+        byte[] finalData = outputStream.toByteArray();
+        String encodedFinalData = DatatypeConverter.printBase64Binary(finalData);
+        return encodedFinalData;
+    }
 
-  public String decrypt(byte[] cipherText, String encryptionKey, String IV) throws Exception{
-    Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
-    SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
-    cipher.init(Cipher.DECRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
-    return new String(cipher.doFinal(cipherText),"UTF-8");
-  }
+    public static String decrypt(String encodedInitialData, String key) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException {
+        byte[] encryptedData = DatatypeConverter.parseBase64Binary(encodedInitialData);
+        byte[] raw = DatatypeConverter.parseHexBinary(key);
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        Cipher cipher = Cipher.getInstance(ALGORITMO);
+        byte[] iv = Arrays.copyOfRange(encryptedData, 0, 16);
+        byte[] cipherText = Arrays.copyOfRange(encryptedData, 16, encryptedData.length);
+        IvParameterSpec iv_specs = new IvParameterSpec(iv);
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv_specs);
+        byte[] plainTextBytes = cipher.doFinal(cipherText);
+        String plainText = new String(plainTextBytes);
+        return plainText;
+    }
 }
