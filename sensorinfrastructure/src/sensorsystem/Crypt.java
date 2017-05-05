9041dca973192bb2a226f84124c082e81ea0fc69
Niklas Buhl <nibuh@dtu.dk>
2017-05-05 16:42:27 +0200

    opdateret michael?

--- sensorinfrastructure/src/sensorsystem/Crypt.java
+++ sensorinfrastructure/src/sensorsystem/Crypt.java
@@ -26,33 +26,7 @@ import javax.xml.bind.DatatypeConverter;
  * @author triblex
  */
 public class Crypt {
-<<<<<<< HEAD
 
-    public byte[] encrypt(String plainText, String encryptionKey, String IV) throws Exception {
-
-        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
-    
-        SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
-    
-        cipher.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
-    
-        return cipher.doFinal(plainText.getBytes("UTF-8"));
-    
-    }
-
-    public String decrypt(byte[] cipherText, String encryptionKey, String IV) throws Exception{
-    
-        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
-        
-        SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
-    
-        cipher.init(Cipher.DECRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
-    
-        return new String(cipher.doFinal(cipherText),"UTF-8");
-  
-    }
-
-=======
     private static final String ALGORITMO = "AES/CBC/PKCS5Padding";
     private static final String CODIFICACION = "UTF-8";
     
@@ -84,5 +58,4 @@ public class Crypt {
         String plainText = new String(plainTextBytes);
         return plainText;
     }
->>>>>>> origin/master
 }
