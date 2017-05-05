de8644ec2775b727a4b1f4ab53eb3c9e38e1fdf0
Niklas Buhl <nibuh@dtu.dk>
2017-05-05 16:39:12 +0200

    dunno

--- sensorinfrastructure/src/sensorsystem/Crypt.java
+++ sensorinfrastructure/src/sensorsystem/Crypt.java
@@ -14,17 +14,29 @@ import javax.crypto.spec.SecretKeySpec;
  * @author triblex
  */
 public class Crypt {
-    public byte[] encrypt(String plainText, String encryptionKey, String IV) throws Exception {
-    Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
-    SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
-    cipher.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
-    return cipher.doFinal(plainText.getBytes("UTF-8"));
-  }
 
-  public String decrypt(byte[] cipherText, String encryptionKey, String IV) throws Exception{
-    Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
-    SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
-    cipher.init(Cipher.DECRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
-    return new String(cipher.doFinal(cipherText),"UTF-8");
-  }
+    public byte[] encrypt(String plainText, String encryptionKey, String IV) throws Exception {
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
+    
+        SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
+    
+        cipher.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+    
+        return cipher.doFinal(plainText.getBytes("UTF-8"));
+    
+    }
+
+    public String decrypt(byte[] cipherText, String encryptionKey, String IV) throws Exception{
+    
+        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
+        
+        SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
+    
+        cipher.init(Cipher.DECRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+    
+        return new String(cipher.doFinal(cipherText),"UTF-8");
+  
+    }
+
 }
